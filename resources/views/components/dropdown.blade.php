6b4611a6d40a5b521d921acad9ec3a217acc2914
Josua P. Sianturi <josuapsianturi@gmail.com>
2025-04-15 15:00:32 +0700

    Update dropdown.blade.php

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,32 +1,48 @@
-<div x-cloak class="inline-block relative" x-data="{ open: false }">
-    <button
-        @click="open = !open"
-        @click.away="open = false"
-        class="focus:outline-none shadow cursor-pointer inline-block text-gray-700 hover:text-gray-900 flex border border-gray-200 rounded p-2 pl-3 pr-1 bg-gray-100"
-        :class="{ 'shadow-none border-gray-300': open } {{ $buttonClass ?? '' }}"
-    >
-        {{ $label }}
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" :class="{'-rotate-180': open}"
-            class="ml-1 transform duration-200 inline-block fill-current text-gray-500 w-6 h-6">
-            <path fill-rule="evenodd" d="M15.3 10.3a1 1 0 011.4 1.4l-4 4a1 1 0 01-1.4 0l-4-4a1 1 0 011.4-1.4l3.3 3.29 3.3-3.3z" />
-        </svg>
-    </button>
+@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white', 'dropdownClasses' => ''])
 
-    <ul
-        x-show="open"
-        class="bg-white absolute left-0 shadow w-full rounded text-gray-600 origin-top shadow-lg"
-        x-transition:enter="transition ease-out duration-200"
-        x-transition:enter-start="opacity-0 transform scale-y-50"
-        x-transition:enter-end="opacity-100 transform scale-y-100"
-        x-transition:leave="transition ease-in duration-200"
-        x-transition:leave-end="opacity-0 transform scale-y-50"
+@php
+    switch ($align) {
+        case 'left':
+            $alignmentClasses = 'origin-top-left left-0';
+            break;
+        case 'top':
+            $alignmentClasses = 'origin-top';
+            break;
+        case 'none':
+        case 'false':
+            $alignmentClasses = '';
+            break;
+        case 'right':
+        default:
+            $alignmentClasses = 'origin-top-right right-0';
+            break;
+    }
+
+    switch ($width) {
+        case '48':
+            $width = 'w-48';
+            break;
+    }
+@endphp
+
+<div class="relative" x-data="{ open: false }" @click.away="open = false" @close.stop="open = false">
+    <div @click="open = ! open">
+        {{ $trigger }}
+    </div>
+
+    <div x-show="open"
+         x-transition:enter="transition ease-out duration-200"
+         x-transition:enter-start="transform opacity-0 scale-95"
+         x-transition:enter-end="transform opacity-100 scale-100"
+         x-transition:leave="transition ease-in duration-75"
+         x-transition:leave-start="transform opacity-100 scale-100"
+         x-transition:leave-end="transform opacity-0 scale-95"
+         class="absolute z-40 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }} {{ $dropdownClasses }}"
+         style="display: none;"
+         @click="open = false"
     >
-        @foreach ($items as $item)
-            <li>
-                <a href="{{ $item['href'] }}" class="py-1 px-3 border-b block hover:bg-gray-200 {{ $item['class'] ?? '' }}">
-                    {{ $item['label'] }}
-                </a>
-            </li>
-        @endforeach
-    </ul>
+        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
+            {{ $content }}
+        </div>
+    </div>
 </div>
