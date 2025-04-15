4a22f3d1ee1fe3310126a3ff1ba2848595172935
Josua P. Sianturi <josuapsianturi@gmail.com>
2025-04-15 09:52:22 +0700

    Update LaravelTacServiceProvider.php

--- src/LaravelTacServiceProvider.php
+++ src/LaravelTacServiceProvider.php
@@ -28,5 +28,9 @@ class LaravelTacServiceProvider extends PackageServiceProvider
     {
         // Register component
         Blade::component('tac::components.dropdown', 'tac-dropdown');
+    
+           $this->publishes([
+            __DIR__.'/../resources/views/components' => resource_path('views/vendor/tac/components'),
+        ], 'laravel-tac-components');
     }
 }
